45ad31adf381fc6e8556f23d9fc93677ef85a76e
Diego Hernández <redbandito@outlook.com>
2023-06-24 01:07:17 -0600

    Implementation of custom validations Use of more descriptive route names for nutrition goals

---
+++ Server/helpers/db-validators.js
@@ -0,0 +1,114 @@
+const { Category, Role, User, Product, Household } = require('../models');
+
+/**
+ *
+ * @param {String} role
+ */
+const roleIsValid = async (role) => {
+    const roleExists = await Role.findOne({ role });
+
+    if (!roleExists) {
+        throw new Error(`Role '${role}' is not recognized`);
+    }
+};
+
+/**
+ *
+ * @param {String} email
+ */
+const emailExists = async (email) => {
+    const emailExists = await User.findOne({ email });
+    if (emailExists) {
+        throw new Error(`Email: ${email} already has an account`);
+    }
+};
+
+/**
+ *
+ * @param {String} id
+ */
+const userExistsById = async (id) => {
+    const userExists = await User.findById(id);
+    if (!userExists) {
+        throw new Error(`Id: ${id} doesn't exist`);
+    }
+};
+
+/**
+ *
+ * @param {String} id
+ */
+const userBelongsToHousehold = async (id) => {
+    const user = await User.findById(id);
+    const household = await Household.findOne({ members: id });
+    if (!user.household || !household) {
+        throw new Error(`User: ${id} doesn't belong to a household`);
+    }
+};
+//TODO: find real use xd
+/**
+ *
+ * @param {String} id
+ */
+const isHouseholdAdmin = async (id) => {
+    const household = await Household.findOne({ admins: id });
+    if (!household) {
+        throw new Error(`User: ${id} is not a household's admin`);
+    }
+};
+
+/**
+ *
+ * @param {String} id
+ */
+const householdExistsById = async (id) => {
+    const householdExists = await Household.findById(id);
+    if (!householdExists) {
+        throw new Error(`Household Id: ${id} doesn't exist`);
+    }
+};
+
+//TODO: refactor to fit project
+/**
+ *
+ * @param {String} id
+ */
+const categoryExists = async (id) => {
+    const categoryExists = await Category.findById(id);
+    if (!categoryExists) {
+        throw new Error(`Id: ${id} doesn't exist`);
+    }
+};
+/**
+ *
+ * @param {String} name
+ */
+const categoryExistsByName = async (name = '') => {
+    name = name.toUpperCase();
+    const categoryExists = await Category.findOne({ name });
+    if (!categoryExists) {
+        throw new Error(`Id: ${id} doesn't exist`);
+    }
+};
+/**
+ *
+ * @param {String} id
+ */
+const productExists = async (id) => {
+    const productExists = await Product.findById(id);
+    if (!productExists) {
+        throw new Error(`Id: ${id} doesn't exist`);
+    }
+};
+
+module.exports = {
+    categoryExists,
+    categoryExistsByName,
+    emailExists,
+    householdExistsById,
+    isHouseholdAdmin,
+    productExists,
+    roleIsValid,
+    userBelongsToHousehold,
+    userExistsById,
+};
--- Server/routes/auth.routes.js
+++ Server/routes/auth.routes.js
@@ -7,6 +7,12 @@ const {
     tokenRenew,
     userLogin,
 } = require('../controllers');
+const {
+    emailExists,
+    userExistsById,
+    userBelongsToHousehold,
+    householdExistsById,
+} = require('../helpers/db-validators');
 const { validateJWT, fieldValidation } = require('../middlewares');
 
 const mailExp = new RegExp(/^[( \w \. \-)]{4,20}@((g|hot)mail|outlook|live).com$/);
@@ -17,7 +23,8 @@ const router = Router();
 router.post(
     '/register',
     [
-        check('email', 'Email is not optional').matches(mailExp),
+        check('email', 'Enter a valid email').isEmail().matches(mailExp),
+        check('email', 'User already exists').custom(emailExists),
         check('password', 'Password is not optional').isLength({ min: 6 }),
         check('name', 'Name is not optional').not().isEmpty(),
         check('userName', 'User name is not optional').not().isEmpty(),
@@ -30,7 +37,8 @@ router.post(
 router.post(
     '/',
     [
-        check('email', 'Enter a valid email').optional().matches(mailExp),
+        check('email', 'Enter a valid email').isEmail().matches(mailExp),
+        check('email', 'User does not exist').not().custom(emailExists),
         check('password', 'Password is not optional').isLength({ min: 6 }),
         fieldValidation,
     ],
@@ -42,10 +50,15 @@ router.post(
     '/household',
     [
         check('name', 'Household name is not optional').not().isEmpty(),
-        check('requestorId', 'Requestor ID is not optional').isMongoId(),
+        check('requestorId', 'Please enter a valid id').isMongoId(),
+        check('requestorId', 'The requesting user does not exist').custom(userExistsById),
+        check('requestorId', 'The requesting user is already in a household')
+            .not()
+            .custom(userBelongsToHousehold),
         check('lowLevel', 'Insert a valid value: (0-100)')
             .optional()
             .isInt({ min: 0, max: 100 }),
+        //TODO: add currency validation
         check('currency', 'Insert a valid currency').optional(),
         check('budget', 'Insert a valid budget').optional().isInt({ min: 0 }),
         fieldValidation,
@@ -57,22 +70,44 @@ router.post(
 router.post(
     '/c-nutritiongoals',
     [
-        check('requestorId', 'Requestor ID is not optional').isMongoId(),
+        check('requestorId', 'Please enter a valid id').isMongoId(),
+        check('requestorId', 'The requesting user does not exist').custom(userExistsById),
         check('householdId', 'Household ID is not optional').isMongoId(),
-        check('date', 'Insert a valid starting date (tomorrow)').isDate().isAfter(),
+        check('householdId', 'The household does not exist').custom(householdExistsById),
+        check(
+            'requestorId',
+            'The user does not belong to this household or is not an admin'
+        ).custom(userBelongsToHousehold),
+        check('startingDate', 'Insert a valid starting date (tomorrow)').isDate().isAfter(),
         check('period', 'Insert a valid period').isIn(['daily', 'weekly', 'monthly']),
-        check('cal', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('protein', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('carbs', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('fat', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('fiber', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('sugar', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('sodium', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('cholesterol', 'Insert a numeric value').optional().isInt({ min: -1 }),
-        check('animalBalanceGoal', 'Insert a numeric value')
+        check('totalPeriodCalories', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodProtein', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodCarbs', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodFat', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodFiber', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodSugar', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodSodium', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('totalPeriodCholesterol', 'Insert 0 or positive numeric value')
+            .optional()
+            .isInt({ min: -1 }),
+        check('animalBalanceGoal', 'Insert 0 or positive numeric value')
             .optional()
             .isInt({ min: 0, max: 100 }),
-        check('vegetalBalanceGoal', 'Insert a numeric value')
+        check('vegetalBalanceGoal', 'Insert 0 or positive numeric value')
             .optional()
             .isInt({ min: 0, max: 100 }),
         fieldValidation,
